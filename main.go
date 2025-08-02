a564975eac25364313261c4b0e2af9d4867bcdd6
0xTheFr34k <tayayassine6@live.fr>
2025-08-02 17:13:02 +0100

    setup each route with own middlerwares

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/0xTheFr34k/go-net-http/handler/auth"
-	// "github.com/0xTheFr34k/go-net-http/handler/user"
+	"github.com/0xTheFr34k/go-net-http/handler/user"
 	"github.com/0xTheFr34k/go-net-http/middleware"
+	"net/http"
 )
 
 func main() {
 
-	authMux := http.NewServeMux()
+	appMux := http.NewServeMux()
 
-	stack := middleware.CreateStack(
+	authMux := http.NewServeMux()
+	authMux.HandleFunc("/login", auth.Login)
+
+	authstack := middleware.CreateStack(
 		middleware.AuthMiddleware,
 		middleware.Logging,
 	)
-	authMux.HandleFunc("/login", auth.Login)
-	http.ListenAndServe(":8000", stack(authMux))
+
+	appMux.Handle("/auth/", http.StripPrefix("/auth", authstack(authMux)))
+
+	userMux := http.NewServeMux()
+	userMux.HandleFunc("/{id}", user.GetUserByID)
+
+	userstack := middleware.CreateStack(
+		middleware.AuthMiddleware,
+		middleware.Logging,
+	)
+
+	appMux.Handle("/user/", http.StripPrefix("/user", userstack(userMux)))
+
+	versionMux := http.NewServeMux()
+
+	versionMux.Handle("/v1/", http.StripPrefix("/v1", appMux))
+
+	http.ListenAndServe(":8000", versionMux)
 }
