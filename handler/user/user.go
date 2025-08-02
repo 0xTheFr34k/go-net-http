80c9347c443d53334e0d22cd98c4316939ce7e22
0xTheFr34k <tayayassine6@live.fr>
2025-08-02 19:10:40 +0100

    return json in response

--- handler/user/user.go
+++ handler/user/user.go
@@ -1,21 +1,38 @@
 package user
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"strconv"
 )
+
+type Response struct {
+	Message    string `json:"message"`
+	CodeStatus int    `json:"code"`
+}
 
 func GetUserByID(w http.ResponseWriter, r *http.Request) {
 	_id := r.PathValue("id")
 
 	id, err := strconv.Atoi(_id)
+
+	w.Header().Set("Content-Type", "application/json")
+
 	if err != nil || id < 0 {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(
+			Response{
+				Message:    "Error",
+				CodeStatus: http.StatusBadRequest,
+			})
 		return
 	}
 
-	fmt.Fprintf(w, "Your ID is %s\n", _id)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Response{
+		Message:    "Success",
+		CodeStatus: http.StatusOK,
+	})
 }
 
 func Panic(w http.ResponseWriter, r *http.Request) {
